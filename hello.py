960c6a47574caf4577aea2424d1ade4102676f8c
sjaramilloo <segara968@gmail.com>
2022-05-27 18:12:35 -0500

    bug fix

--- hello.py
+++ hello.py
@@ -1,21 +1,26 @@
+from lib2to3.pgen2 import driver
 import unittest
 from pyunitreport import HTMLTestRunner
 from selenium import webdriver
 
-class helloworld(unittest.TestCase):
+class Helloworld(unittest.TestCase):
     @classmethod
-    def setUpClass(self):
-        self.driver = webdriver.Chrome(executable_path = r'D:\Users\USUARIO\Desktop\Tec\pruebaSelenium\chromedriver.exe')
-        driver = self.driver
-        driver.implicitly_wait(10)
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path = r'D:\Users\USUARIO\Desktop\Tec\pruebaSelenium\chromedriver.exe')
+        driver = cls.driver
+        driver.implicitly_wait(4)
 
     def test_hello_world(self):
         driver = self.driver
         driver.get('https://www.platzi.com')
 
-    def tearDown(self):
-        self.driver.quit()
+    def test_visit_wiki(self):
+        self.driver.get('https://www.wikipedia.org')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
     
-    if __name__ == '__main__':
-        unittest.main(verbosity = 2, testRunner= HTMLTestRunner(output = 'reportes',report_name = 'hello-world-report'))
+if __name__ == "__main__":
+    unittest.main(verbosity = 2, testRunner= HTMLTestRunner(output = 'reportes',report_name = 'hello-world-report'))
     
