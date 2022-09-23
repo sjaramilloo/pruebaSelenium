da80e944abff58bcaa6360cf9667d2ef6c5354a7
SergioUribeHD <sergio.uribe@hemisferiod.co>
2022-09-22 19:53:35 -0500

    update test Hello

--- hello.py
+++ hello.py
@@ -1,26 +1,18 @@
 from lib2to3.pgen2 import driver
-import unittest
-from pyunitreport import HTMLTestRunner
+from re import search
+import time
+from webbrowser import Chrome
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-class Helloworld(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.driver = webdriver.Chrome(executable_path = r'D:\Users\USUARIO\Desktop\Tec\pruebaSelenium\chromedriver.exe')
-        driver = cls.driver
-        driver.implicitly_wait(4)
+driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-    def test_hello_world(self):
-        driver = self.driver
-        driver.get('https://www.platzi.com')
-
-    def test_visit_wiki(self):
-        self.driver.get('https://www.wikipedia.org')
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.driver.quit()
-    
-if __name__ == "__main__":
-    unittest.main(verbosity = 2, testRunner= HTMLTestRunner(output = 'reportes',report_name = 'hello-world-report'))
-     
+driver.get('http://www.google.com/')
+time.sleep(5)
+search_box= driver.find_element(By.NAME,'q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5)
+driver.quit()
